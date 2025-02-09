850af9bd3b9d80ae06a0dbbd3baeb4eec9fcdc27
Shankar <sivashankarsubramaniam@gmail.com>
2025-02-09 18:16:02 +0530

    Project Structure Commit

---
+++ template.py
@@ -0,0 +1,85 @@
+import os 
+from pathlib import Path  
+import logging 
+
+
+project_name= "MLOps_Project"  
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= '[%(asctime)s]: %(message)s:',
+    handlers=[ 
+        logging.StreamHandler()
+    ]
+)
+
+
+file_names= [ 
+    ".github/workflows/.gitkeep", 
+    f"src/{project_name}/__init__.py",  
+    f"src/{project_name}/hi.py", 
+    f"src/{project_name}/components/__init__.py", 
+    f"src/{project_name}/utils/__init__.py", 
+    f"src/{project_name}/utils/common.py",   
+    f"src/{project_name}/config/__init__.py", 
+    f"src/{project_name}/config/configuration.py", 
+    f"src/{project_name}/pipeline/__init__.py", 
+    f"src/{project_name}/entity/__init__.py", 
+    f"src/{project_name}/entity/config_entity.py", 
+    f"src/{project_name}/constants/__init__.py", 
+    "config/config.yaml", 
+    "params.yaml", 
+    "schema.yaml",
+    "main.py", 
+    "Dockerfile", 
+    "requirements.txt", 
+    "setup.py", 
+    "research.ipynb", 
+    "templates/index.html", 
+
+    "app.py"
+]
+
+
+
+for filepath in file_names: 
+    filepath= Path(filepath)
+    filedir, filename= os.path.split(filepath)
+    
+
+    if os.path.exists(filepath):  
+        logging.info(f"{filename} in already exists inside {filepath}")  
+    
+
+    else:
+
+        if os.path.exists(filedir) or filedir=="":  
+            print(filepath)
+            with open(filepath, "w") as file: 
+                
+                pass  
+                logging.info(f"Creating empty file {filepath}")    
+
+
+        else: 
+            os.makedirs(filedir, exist_ok=True) 
+            logging.info(f"Creating directory {filedir} for the file {filename}")  
+
+            with open(filepath, "w") as file: 
+                
+                pass  
+                logging.info(f"Creating empty file {filepath}")    
+
+        
+
+ 
+
+
+
+
+
+        
+    
+
+
